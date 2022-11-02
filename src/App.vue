ade1565e7e5321326a1da5f23c82fdb4e57a3c60
martindonadieu <martindonadieu@gmail.com>
2022-11-02 16:05:36 +0800

    fix: commented code

--- src/App.vue
+++ src/App.vue
@@ -7,29 +7,30 @@
 <script lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
-  // import { CapacitorUpdater } from 'capacitor-updater'
-  // import { SplashScreen } from '@capacitor/splash-screen'
-  // import { App } from '@capacitor/app'
+// import { BundleInfo, CapacitorUpdater } from '@capgo/capacitor-updater'
+// import { SplashScreen } from '@capacitor/splash-screen'
+// import { App } from '@capacitor/app'
 
-  // let data = {version: ""}
-  // App.addListener('appStateChange', async(state) => {
-  //     if (state.isActive) {
-  //       // Do the download during user active app time to prevent failed download
-  //       data = await CapacitorUpdater.download({
-  //       url: 'https://github.com/Cap-go/demo-app/releases/download/0.0.4/dist.zip',
-  //       })
-  //     }
-  //     if (!state.isActive && data.version !== "") {
-  //       // Do the switch when user leave app
-  //       SplashScreen.show()
-  //       try {
-  //         await CapacitorUpdater.set(data)
-  //       } catch (err) {
-  //         console.log(err)
-  //         SplashScreen.hide() // in case the set fail, otherwise the new app will have to hide it
-  //       }
-  //     }
-  // })
+// let data: BundleInfo | null = null
+// App.addListener('appStateChange', async(state) => {
+//     if (state.isActive) {
+//       // Do the download during user active app time to prevent failed download
+//       data = await CapacitorUpdater.download({
+//         url: 'https://github.com/Cap-go/demo-app/releases/download/0.0.4/dist.zip',
+//         version: '0.0.4',
+//       })
+//     }
+//     if (!state.isActive && data) {
+//       // Do the switch when user leave app
+//       SplashScreen.show()
+//       try {
+//         await CapacitorUpdater.set({id: data.version})
+//       } catch (err) {
+//         console.log(err)
+//         SplashScreen.hide() // in case the set fail, otherwise the new app will have to hide it
+//       }
+//     }
+// })
 
 export default defineComponent({
   name: 'App',
