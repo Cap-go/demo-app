e9c73761a5b7a5d25096128bccefbde371d0f425
Martin Donadieu <martindonadieu@gmail.com>
2023-04-08 04:17:53 +0300

    fix: remove jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,15 +17,4 @@ module.exports = {
     'vue/no-deprecated-slot-attribute': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
 }
