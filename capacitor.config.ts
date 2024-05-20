82e408d4c915f57cd2f8a7cb7648160430a1d0b0
WcaleNieWolny <isupermichael007@gmail.com>
2024-05-20 06:24:56 +0200

    🎉 (chroe): Init

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'capgoTestApp',
+  appId: 'me.wcaleniewolny.test.ionic.vue',
+  appName: 'capgoTestVueApp',
   webDir: 'dist'
 };
 
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,20 +2,20 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Capgo cloud demo</ion-title>
       </ion-toolbar>
     </ion-header>
 
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
+          <ion-title size="large">Capgo cloud demo</ion-title>
         </ion-toolbar>
       </ion-header>
 
       <div id="container">
         <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <p>Start with Capgo Cloud!</p>
       </div>
     </ion-content>
   </ion-page>
