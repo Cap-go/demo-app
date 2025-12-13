9d312bd78f067ad3020584c66147b2c36b195ebb
Martin Donadieu <martindonadieu@gmail.com>
2025-12-13 15:58:13 +0000

    feat: Update iOS project configuration and dependencies
    - Added IDEWorkspaceChecks.plist to suppress Mac 32-bit warnings.
    - Updated AppDelegate.swift to include URL handling for social login.
    - Modified Info.plist to include CAPACITOR_DEBUG key and removed deprecated Facebook configurations.
    - Created .gitignore for CapApp-SPM to exclude unnecessary files.
    - Added Package.swift for Swift Package Manager dependencies.
    - Included README.md for CapApp-SPM with usage instructions.
    - Implemented CapApp-SPM.swift to define isCapacitorApp flag.
    - Removed outdated Podfile and Podfile.lock as part of migration to SPM.
    - Added debug.xcconfig to manage debug configurations.
    - Updated package.json to reflect new versions of dependencies and improved compatibility.

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -10,11 +10,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-//        FBSDKCoreKit.ApplicationDelegate.shared.application(
-//            application,
-//            didFinishLaunchingWithOptions: launchOptions
-//        )
-
+        //        FBSDKCoreKit.ApplicationDelegate.shared.application(
+        //            application,
+        //            didFinishLaunchingWithOptions: launchOptions
+        //        )
         return true
     }
 
@@ -38,6 +37,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+        // Called when the app was launched with a url. Feel free to add additional processing here,
+        // but if you want the App API to support tracking app url opens, make sure to keep this call
+        return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
     }
 
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
---
+++ ios/App/CapApp-SPM/Package.swift
@@ -0,0 +1,55 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+// DO NOT MODIFY THIS FILE - managed by Capacitor CLI commands
+let package = Package(
+    name: "CapApp-SPM",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "CapApp-SPM",
+            targets: ["CapApp-SPM"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.0.0"),
+        .package(name: "CapacitorApp", path: "../../../node_modules/@capacitor/app"),
+        .package(name: "CapacitorDevice", path: "../../../node_modules/@capacitor/device"),
+        .package(name: "CapacitorFilesystem", path: "../../../node_modules/@capacitor/filesystem"),
+        .package(name: "CapacitorHaptics", path: "../../../node_modules/@capacitor/haptics"),
+        .package(name: "CapacitorKeyboard", path: "../../../node_modules/@capacitor/keyboard"),
+        .package(name: "CapacitorSplashScreen", path: "../../../node_modules/@capacitor/splash-screen"),
+        .package(name: "CapacitorStatusBar", path: "../../../node_modules/@capacitor/status-bar"),
+        .package(name: "CapgoCapacitorDataStorageSqlite", path: "../../../node_modules/@capgo/capacitor-data-storage-sqlite"),
+        .package(name: "CapgoCapacitorScreenRecorder", path: "../../../node_modules/@capgo/capacitor-screen-recorder"),
+        .package(name: "CapgoCapacitorShake", path: "../../../node_modules/@capgo/capacitor-shake"),
+        .package(name: "CapgoCapacitorSocialLogin", path: "../../../node_modules/@capgo/capacitor-social-login"),
+        .package(name: "CapgoCapacitorUploader", path: "../../../node_modules/@capgo/capacitor-uploader"),
+        .package(name: "CapgoInappbrowser", path: "../../../node_modules/@capgo/inappbrowser"),
+        .package(name: "CapgoNativeAudio", path: "../../../node_modules/@capgo/native-audio"),
+        .package(name: "CapgoNativePurchases", path: "../../../node_modules/@capgo/native-purchases")
+    ],
+    targets: [
+        .target(
+            name: "CapApp-SPM",
+            dependencies: [
+                .product(name: "Capacitor", package: "capacitor-swift-pm"),
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "CapacitorApp", package: "CapacitorApp"),
+                .product(name: "CapacitorDevice", package: "CapacitorDevice"),
+                .product(name: "CapacitorFilesystem", package: "CapacitorFilesystem"),
+                .product(name: "CapacitorHaptics", package: "CapacitorHaptics"),
+                .product(name: "CapacitorKeyboard", package: "CapacitorKeyboard"),
+                .product(name: "CapacitorSplashScreen", package: "CapacitorSplashScreen"),
+                .product(name: "CapacitorStatusBar", package: "CapacitorStatusBar"),
+                .product(name: "CapgoCapacitorDataStorageSqlite", package: "CapgoCapacitorDataStorageSqlite"),
+                .product(name: "CapgoCapacitorScreenRecorder", package: "CapgoCapacitorScreenRecorder"),
+                .product(name: "CapgoCapacitorShake", package: "CapgoCapacitorShake"),
+                .product(name: "CapgoCapacitorSocialLogin", package: "CapgoCapacitorSocialLogin"),
+                .product(name: "CapgoCapacitorUploader", package: "CapgoCapacitorUploader"),
+                .product(name: "CapgoInappbrowser", package: "CapgoInappbrowser"),
+                .product(name: "CapgoNativeAudio", package: "CapgoNativeAudio"),
+                .product(name: "CapgoNativePurchases", package: "CapgoNativePurchases")
+            ]
+        )
+    ]
+)
